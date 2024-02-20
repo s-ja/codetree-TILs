dcff9bbc6436351749d18475a0acfebf7b136a7a
ASJ <dkstmdwl0615@naver.com>
2024-02-20 18:21:00 +0900

    Title: 길이 단위 환산하기, Time: 180ms, Memory: 20MB, Status: Passed - CodeTree

--- 240220/길이 단위 환산하기/convert-length-units.js
+++ 240220/길이 단위 환산하기/convert-length-units.js
@@ -4,4 +4,5 @@ let input = fs.readFileSync(0).toString();
 
 input = Number(input)
 
-console.log(Math.round((input*30.48)*10)/10)
+const round = (Math.round((input*30.48)*10)/10)
+console.log(round.toFixed(1))
