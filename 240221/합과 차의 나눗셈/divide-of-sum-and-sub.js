a96abd63beb10f2551a1bab96b6e73993580f7a6
ASJ <dkstmdwl0615@naver.com>
2024-02-21 19:39:15 +0900

    Title: 합과 차의 나눗셈, Time: 171ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240221/합과 차의 나눗셈/divide-of-sum-and-sub.js
@@ -0,0 +1,13 @@
+const fs = require("fs");
+
+let input = fs.readFileSync(0).toString().trim().split(" ");
+
+const a = Number(input[0])
+const b = Number(input[1])
+
+const plus = a + b
+const minus = a - b
+
+const result = Math.round((plus/minus)*100)/100
+
+console.log(result)
