9ffc141d05e2daad340a45418ad7da9c879c881b
ASJ <dkstmdwl0615@naver.com>
2024-02-22 15:51:33 +0900

    Title: 최댓값 구하기, Time: 160ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240222/최댓값 구하기/maximum-value.js
+++ 240222/최댓값 구하기/maximum-value.js
@@ -4,5 +4,19 @@ let input = fs.readFileSync(0).toString().trim().split(' ');
 const a = Number(input[0])
 const b = Number(input[1])
 const c = Number(input[2])
+const arr = [a,b,c]
+// console.log(Math.max(a,b,c))
 
-console.log(Math.max(a,b,c))
+let result = 0
+for(let i=0; i<arr.length; i++){
+    if(arr[i+1]===undefined){
+        break
+    }
+    if(arr[i]>arr[i+1]){
+        result = arr[i]
+    }else{
+        result = arr[i+1]
+    }
+}
+
+console.log(result)
