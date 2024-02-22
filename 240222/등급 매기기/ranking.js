2e6dddc26b34e19c1097e4db685771cbc6336eb7
ASJ <dkstmdwl0615@naver.com>
2024-02-22 12:52:02 +0900

    Title: 등급 매기기, Time: 194ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240222/등급 매기기/ranking.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+let input = fs.readFileSync("/dev/stdin").toString().trim();
+input = Number(input)
+if(input >= 90){
+    console.log('A')
+}else if(input >= 80){
+    console.log('B')
+}else if(input >= 70){
+    console.log('C')
+} else if(input < 60){
+    console.log('D')
+}
