37d597395dfcbb8be599df6c1dfadffd73c79b45
ASJ <dkstmdwl0615@naver.com>
2024-02-22 14:34:34 +0900

    Title: 두 사람, Time: 236ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240222/두 사람/two-person.js
+++ 240222/두 사람/two-person.js
@@ -9,12 +9,28 @@ c = Number(c)
 
 // console.log(a,b,c,d)
 
-if(a>=19 || c>19){
-    if(b==='M'||d==='M'){
+// if(a>=19 || c>19){
+//     if(b==='M'|| d==='M'){
+//         console.log(1)
+//     }else{
+//         console.log(0)
+//     }
+// }else {
+//     console.log(0)
+//     }
+
+if(a>=19){
+    if(b === 'M'){
         console.log(1)
+    }else if(c >= 19){
+        if(d === 'M'){
+            console.log(1)
+        }else{
+            console.log(0)
+        }
     }else{
         console.log(0)
     }
-}else {
+}else{
     console.log(0)
-    }
+}
