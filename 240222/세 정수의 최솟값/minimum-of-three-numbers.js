30d179eba32c10a0ed614673e9eebb40cf21b70c
ASJ <dkstmdwl0615@naver.com>
2024-02-22 13:52:44 +0900

    Title: 세 정수의 최솟값, Time: 225ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240222/세 정수의 최솟값/minimum-of-three-numbers.js
@@ -0,0 +1,8 @@
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().trim().split(" ");
+
+const a = Number(input[0])
+const b = Number(input[1])
+const c = Number(input[2])
+
+console.log(Math.min(a,b,c))
