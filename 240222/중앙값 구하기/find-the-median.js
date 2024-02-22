8218151c689afaf2b7bb15ec7d10f46c49412c48
ASJ <dkstmdwl0615@naver.com>
2024-02-22 16:43:34 +0900

    Title: 중앙값 구하기, Time: 183ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240222/중앙값 구하기/find-the-median.js
+++ 240222/중앙값 구하기/find-the-median.js
@@ -7,6 +7,8 @@ input.forEach((i)=>{
     arr.push(Number(i))
 })
 
+// let result = []
+
 let result = [arr[0]]
 
 for(let i = 0; i<arr.length; i++){
@@ -21,3 +23,18 @@ for(let i = 0; i<arr.length; i++){
 }
 
 console.log(result[1])
+
+// if(arr[0]<arr[1]){
+//     if(arr[0]<arr[2]){
+//         result.push(arr[0])
+//     }else if(arr[1]<arr[2]){
+//         result.push(arr[1])
+//     }else{
+//         result.push(arr[2])
+//     }
+// }else{
+    
+// }
+
+
+// console.log(result[1])
