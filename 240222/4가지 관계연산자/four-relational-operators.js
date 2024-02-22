a810726002977f5456dd3dc0eb74dadd2e5b8b0f
ASJ <dkstmdwl0615@naver.com>
2024-02-22 13:20:40 +0900

    Title: 4가지 관계연산자, Time: 253ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240222/4가지 관계연산자/four-relational-operators.js
@@ -0,0 +1,29 @@
+const fs = require("fs");
+let input = fs.readFileSync("/dev/stdin").toString().trim().split(" ");
+
+const a = Number(input[0])
+const b = Number(input[1])
+
+if(a>=b){
+    console.log(1)
+}else{
+    console.log(0)
+}
+
+if(a>b){
+    console.log(1)
+}else{
+    console.log(0)
+}
+
+if(a<=b){
+    console.log(1)
+}else{
+    console.log(0)
+}
+
+if(a<b){
+    console.log(1)
+}else{
+    console.log(0)
+}
