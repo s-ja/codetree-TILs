8744daee5f5f0a859ef006760e3d27eae7826c42
ASJ <dkstmdwl0615@naver.com>
2024-02-22 13:33:56 +0900

    Title: 점수 비교, Time: 219ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240222/점수 비교/score-comparison.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+let input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+const a = input[0].split(" ")
+const b = input[1].split(" ")
+
+// console.log(a,b)
+
+if(Number(a[0])>Number(b[0]) && Number(a[1])>Number(b[1])){
+    console.log(1)
+}
