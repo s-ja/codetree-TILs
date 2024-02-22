8d1839c103cac20770efc8150446768607798def
ASJ <dkstmdwl0615@naver.com>
2024-02-22 13:44:23 +0900

    Title: 특정 조건 세 정수 비교, Time: 194ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240222/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.js
@@ -0,0 +1,20 @@
+const fs = require("fs");
+let input = fs.readFileSync("/dev/stdin").toString().trim().split(" ");
+
+const [a,b,c] = input
+
+let [d,e] = [0,0]
+
+if(a<b && a<c){
+    d=1
+}else{
+    d=0
+}
+
+if(a==b && a==c){
+    e=1
+}else{
+    e=0
+}
+
+console.log(d,e)
