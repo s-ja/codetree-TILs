a8dfc8fa4e2775351a4dde36aa73bb59080c87ab
ASJ <dkstmdwl0615@naver.com>
2024-02-25 16:11:59 +0900

    Title: 윤년 횟수 구하기, Time: 226ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240225/윤년 횟수 구하기/number-of-leap-years.js
@@ -0,0 +1,17 @@
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().trim();
+
+const year = Number(input)
+
+let count = 0
+
+for(let i = 1; i <= year; i++){
+    if(i % 4 === 0){
+        if(i % 100 === 0 && i % 400 !== 0){
+        }else{
+            count++
+        }
+    }
+}
+
+console.log(count)
