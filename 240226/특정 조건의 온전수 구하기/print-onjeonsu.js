ef855decb6639fb515f3c69a9cebbd0c65307832
ASJ <dkstmdwl0615@naver.com>
2024-02-26 12:00:57 +0900

    Title: 특정 조건의 온전수 구하기, Time: 248ms, Memory: 14MB, Status: Passed - CodeTree

--- 240226/특정 조건의 온전수 구하기/print-onjeonsu.js
+++ 240226/특정 조건의 온전수 구하기/print-onjeonsu.js
@@ -5,7 +5,7 @@ const n = Number(input)
 let result = ""
 
 for(let i = 1; i <= n; i++){
-    if(i % 2 === 0 || i.toString()[0] === "5" || (i % 3 === 0 && i % 9 !== 0)){
+    if(i % 2 === 0 || i % 5 === 0 || (i % 3 === 0 && i % 9 !== 0)){
         continue
     }else{
         result += (i + " ")
