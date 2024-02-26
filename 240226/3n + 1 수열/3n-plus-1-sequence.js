46120f8e1b47be90ba956d72d3148296e265fa7d
ASJ <dkstmdwl0615@naver.com>
2024-02-26 18:01:09 +0900

    Title: 3n + 1 수열, Time: 200ms, Memory: 14MB, Status: Passed - CodeTree

--- 240226/3n + 1 수열/3n-plus-1-sequence.js
+++ 240226/3n + 1 수열/3n-plus-1-sequence.js
@@ -5,6 +5,10 @@ let n = Number(input)
 let count = 0;
 
 while(true){
+    if(n === 1){
+        break
+    }
+
     if(n % 2 === 0){
         n = Math.floor(n / 2)
         count++
@@ -12,10 +16,5 @@ while(true){
         n = (n * 3) + 1
         count++
     }
-
-    if(n === 1){
-        break
-    }
 }
-
 console.log(count)
