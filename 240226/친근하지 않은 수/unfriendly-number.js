1ee6ad7ddaabd1eeab6b294507ca77435040db0c
ASJ <dkstmdwl0615@naver.com>
2024-02-26 11:55:58 +0900

    Title: 친근하지 않은 수, Time: 224ms, Memory: 15MB, Status: Passed - CodeTree

---
+++ 240226/친근하지 않은 수/unfriendly-number.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().trim();
+
+const n = Number(input)
+let count = 0
+
+for(let i = 1; i <= n; i++){
+    if(i % 2 === 0 || i % 3 === 0 || i % 5 === 0){
+        continue
+    }else{
+        count++
+    }
+}
+
+console.log(count)
