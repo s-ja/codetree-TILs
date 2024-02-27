4d2b007c4bd569b7cfc2acee7891bcffba0fb910
ASJ <dkstmdwl0615@naver.com>
2024-02-27 15:57:27 +0900

    Title: 합성수 판별기, Time: 194ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240227/합성수 판별기/factorization-discriminator.js
+++ 240227/합성수 판별기/factorization-discriminator.js
@@ -3,7 +3,13 @@ let input = fs.readFileSync(0).toString().trim();
 
 const n = Number(input)
 
-if(n !== 6 || n % 2 !== 0 || n % 3 !== 0){
+// if(n !== 6 || n % 2 !== 0 || n % 3 !== 0){
+//     console.log("N")
+// }else{
+//     console.log("C")
+// }
+
+if(n === 2 || n / 2 > 0 || n / 3 > 0){
     console.log("N")
 }else{
     console.log("C")
