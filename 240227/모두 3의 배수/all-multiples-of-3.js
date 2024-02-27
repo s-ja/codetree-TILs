34f101152a4f9313a99b75915212287340b40778
ASJ <dkstmdwl0615@naver.com>
2024-02-27 17:13:48 +0900

    Title: 모두 3의 배수, Time: 183ms, Memory: 14MB, Status: Passed - CodeTree

--- 240227/모두 3의 배수/all-multiples-of-3.js
+++ 240227/모두 3의 배수/all-multiples-of-3.js
@@ -1,12 +1,10 @@
 const fs = require("fs");
 const input = fs.readFileSync(0).toString().trim().split("\n");
 
-let result = false;
+let result = true;
 
 for(let i = 0; i < input.length; i++){
-    if(Number(input[i]) % 3 === 0){
-        result = true;
-    }else{
+    if(Number(input[i]) % 3 !== 0){
         result = false;
     }
 }
