690093b4da10ef55accd6478c15254161905d486
ASJ <dkstmdwl0615@naver.com>
2024-02-28 13:44:43 +0900

    Title: 별 그리기, Time: 203ms, Memory: 15MB, Status: Passed - CodeTree

--- 240228/별 그리기/star-drawing.js
+++ 240228/별 그리기/star-drawing.js
@@ -11,12 +11,12 @@ for(let i = 0; i < n; i++){
     }
     console.log(str)
 }
-for(let i = 1; i < n; i++){
+for(let i = n; i > 1; i--){
     let str = ""
-    for(let j = 0; j < i; j++){
+    for(let j = 0; j < n - i + 1; j++){
         str += " ";
     }
-    for(let j = 0; j < n - (i * 2) + 2; j++){
+    for(let j = 0; j < (i - 1) * 2 - 1; j++){
         str += "*";
     }
     console.log(str)
