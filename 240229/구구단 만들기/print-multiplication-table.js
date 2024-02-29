2b18fc2d5b1edb89b6dc1e20d29ab6667ab13dc7
ASJ <dkstmdwl0615@naver.com>
2024-02-29 14:54:33 +0900

    Title: 구구단 만들기, Time: 189ms, Memory: 47MB, Status: Wrong Answer - CodeTree

---
+++ 240229/구구단 만들기/print-multiplication-table.js
@@ -0,0 +1,20 @@
+const fs = require("fs");
+const input = fs.readFileSync("/dev/stdin").toString().trim().split(" ");
+
+let a = Number(input[0])
+let b = Number(input[1])
+
+if(b > a){
+    [a,b] = [b,a]
+}
+
+for(let i = 1; i < 10; i++){
+    let str = ""
+    for(let j = a; j >= b; j-=2){
+        str += `${j} * ${i} = ${i*j}`
+        if(j > 2){
+            str += " / "
+        }
+    }
+    console.log(str)
+}
