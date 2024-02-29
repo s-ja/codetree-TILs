549a3ab697ef90012072777a2b69ada111fcf478
ASJ <dkstmdwl0615@naver.com>
2024-02-29 15:47:00 +0900

    Title: 19단 출력, Time: 167ms, Memory: 15MB, Status: Passed - CodeTree

---
+++ 240229/19단 출력/nineteen-times-table.js
@@ -0,0 +1,15 @@
+let str = ""
+
+for(let i = 1; i <= 19; i++){
+    for(let j = 1; j <= 19; j++){
+        if( j % 2 === 0){
+            str += `${i} * ${j} = ${i * j}\n`
+        }else if(j === 19){
+            str += `${i} * ${j} = ${i * j}\n`
+        }else{
+            str += `${i} * ${j} = ${i * j} / `
+        }
+    }
+}
+
+console.log(str)
