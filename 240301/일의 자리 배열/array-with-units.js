1987c58fbb36b6edd609ff405573fb28c773b6a6
ASJ <dkstmdwl0615@naver.com>
2024-03-01 16:22:16 +0900

    Title: 일의 자리 배열, Time: 178ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240301/일의 자리 배열/array-with-units.js
@@ -0,0 +1,25 @@
+const fs = require("fs");
+const input = fs.readFileSync("/dev/stdin").toString().trim().split(" ");
+
+let str = ""
+let arr = Array(10)
+arr[0] = Number(input[0])
+arr[1] = Number(input[1])
+
+for (let i = 2; i < arr.length; i++){
+    arr[i] = arr[i-1] + arr[i-2]
+}
+
+// console.log(arr)
+for(a of arr){
+    if(a > 10){
+        let tmp = a.toString()
+        str += tmp[tmp.length - 1] + " "
+        // console.log(tmp[tmp.length - 1])
+    }else{
+        str += a + " "
+    }
+
+}
+
+console.log(str)
