4aec3e803d5bb7a5d340de869971ad7825b47d58
ASJ <dkstmdwl0615@naver.com>
2024-03-01 18:09:40 +0900

    Title: 나눗셈의 나머지, Time: 195ms, Memory: 14MB, Status: Passed - CodeTree

--- 240301/나눗셈의 나머지/remainder-of-division.js
+++ 240301/나눗셈의 나머지/remainder-of-division.js
@@ -7,15 +7,16 @@ let b = input[1]
 let arr = Array(10).fill(0)
 
 while(a > 1){
-    a = Math.floor(a / b)
+    // console.log(Math.floor(a / b), a%b)
     arr[a % b]++
+    a = Math.floor(a / b)
 }
 
 // console.log(arr)
 let result  = 0
 
 for(a of arr){
-    result += a*a
+    result += a**2
 }
 
 console.log(result)
