38ee66845cc92a7e8720da9913fadf20bc0e0d18
ASJ <dkstmdwl0615@naver.com>
2024-03-01 18:42:35 +0900

    Title: 배열 놀이, Time: 180ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240301/배열 놀이/play-with-array.js
@@ -0,0 +1,31 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split("\n");
+
+const [n, q] = input[0].split(" ").map(Number)
+
+let atom = []
+
+for(a of input[1].split(" ")){
+    atom.push(Number(a))
+}
+
+// console.log(atom)
+
+for(let i = 2; i < q + 2; i++){
+    let tmp = input[i].split(" ").map(Number)
+    if(tmp[0] === 1){
+        console.log(atom[tmp[1] - 1])
+    }else if(tmp[0] === 2){
+        for(a of atom){
+            if(a === tmp[1]){
+                console.log(atom.indexOf(tmp[1]) + 1)
+            }
+        }
+    }else if(tmp[0] === 3){
+        let str = "";
+        for(let i = tmp[1] - 1; i < tmp[2]; i++){
+            str += atom[i] + " "
+        }
+        console.log(str)
+    }
+}
