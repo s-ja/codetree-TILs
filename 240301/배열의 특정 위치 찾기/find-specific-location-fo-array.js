e90dd8905149e1829081e5ba0db42c8a13e16300
ASJ <dkstmdwl0615@naver.com>
2024-03-01 14:57:41 +0900

    Title: 배열의 특정 위치 찾기, Time: 167ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240301/배열의 특정 위치 찾기/find-specific-location-fo-array.js
@@ -0,0 +1,17 @@
+const fs = require("fs");
+const input = fs.readFileSync("/dev/stdin").toString().trim().split(" ");
+
+let sum = 0
+let avg = 0
+let cnt = 0
+
+for(let i = 1; i < input.length; i += 2){
+    sum += Number(input[i])
+}
+
+for(let i = 2; i < input.length; i += 2){
+    avg += Number(input[i])
+    cnt++
+}
+
+console.log(sum, (avg/cnt).toFixed(1))
