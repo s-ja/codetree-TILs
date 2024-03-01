7769b975a6228ee88a10afabbf5bff25ea6fbd09
ASJ <dkstmdwl0615@naver.com>
2024-03-01 18:14:23 +0900

    Title: 특정 위치의 문자, Time: 173ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240301/특정 위치의 문자/char-in-specific-location.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim();
+
+const arr = ['L', 'E', 'B', 'R', 'O', 'S']
+
+let cnt = 0
+
+for(a of arr){
+    if(a === input){
+        console.log(cnt)
+    }else{
+        cnt++
+    }
+}
