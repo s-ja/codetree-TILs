2674d07f130f6882fcf8bab29f5eb1a03580cb2e
ASJ <dkstmdwl0615@naver.com>
2024-03-03 20:21:43 +0900

    Title: n개의 숫자 중 최대 2개, Time: 236ms, Memory: 14MB, Status: Passed - CodeTree

--- 240303/n개의 숫자 중 최대 2개/two-max-of-n-num.js
+++ 240303/n개의 숫자 중 최대 2개/two-max-of-n-num.js
@@ -5,18 +5,26 @@ const n = Number(input[0])
 
 const arr = input[1].split(" ").map(Number)
 
-// let result = [arr[0]]
 
 let first = arr[0]
-let second = null
+let tmp = []
 
 for(let i = 1; i < n; i++){
     if(arr[i] > first){
+        tmp.push(first)
         first = arr[i]
-    }else if(!second ||arr[i] > second){
-        second = arr[i]
+    }else{
+        tmp.push(arr[i])
     }
 }
 
-// console.log(result[0], result[1])
+let second = tmp[0]
+
+for(let i = 1; i < tmp.length; i++){
+    if(tmp[i] > second){
+        second = tmp[i]
+    }
+}
+
+// console.log(tmp)
 console.log(first, second)
