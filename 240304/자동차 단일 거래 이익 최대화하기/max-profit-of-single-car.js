4cfc148af0ea355715843c93fe864cd4bcea9e9b
ASJ <dkstmdwl0615@naver.com>
2024-03-04 13:05:55 +0900

    Title: 자동차 단일 거래 이익 최대화하기, Time: 229ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240304/자동차 단일 거래 이익 최대화하기/max-profit-of-single-car.js
+++ 240304/자동차 단일 거래 이익 최대화하기/max-profit-of-single-car.js
@@ -5,14 +5,38 @@ const n = Number(input[0])
 
 const arr = input[1].split(" ").map(Number)
 
-let result = [0];
-
 const cheap = Math.min(...arr)
+const expensive = Math.max(...arr)
+let profit1 = 0
+let profit2 = 0
 
-for(let i = arr.indexOf(cheap) + 1; i < n; i++){
+for(let i = arr.indexOf(cheap); i <= n; i++){
     if(arr[i] > cheap){
-        result.push(arr[i] - cheap)
+        profit1 = arr[i]
     }
 }
 
-console.log(Math.max(...result))
+for(let i = arr.indexOf(expensive); i >= 0; i--){
+    if(arr[i] < expensive){
+        profit2 = arr[i]
+    }
+}
+
+// console.log(profit1, profit2)
+
+if(profit1 !== 0){
+    profit1 = profit1 - cheap
+}
+if(profit2 !== 0){
+    profit2 = expensive - profit2
+}
+
+
+// console.log(profit1, profit2)
+
+console.log(Math.max(profit1, profit2))
+
+// console.log(arr.indexOf(cheap))
+// console.log(arr.indexOf(expensive))
+// console.log(cheap, expensive)
+// console.log(profit1, profit2)
