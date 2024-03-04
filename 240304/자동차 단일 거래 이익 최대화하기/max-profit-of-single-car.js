7c8e6f0f96e734f1bc1e55ae7ebfd6756f202ed4
ASJ <dkstmdwl0615@naver.com>
2024-03-04 12:29:55 +0900

    Title: 자동차 단일 거래 이익 최대화하기, Time: 180ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240304/자동차 단일 거래 이익 최대화하기/max-profit-of-single-car.js
@@ -0,0 +1,18 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split("\n");
+
+const n = Number(input[0])
+
+const arr = input[1].split(" ").map(Number)
+
+let result = 0;
+
+const cheap = Math.min(...arr)
+
+for(let i = arr.indexOf(cheap); i < n; i++){
+    if(arr[i] > cheap){
+        result = arr[i] - cheap
+    }
+}
+
+console.log(result)
