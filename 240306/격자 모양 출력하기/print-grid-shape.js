e8885680f53bad5d914e42a78dc6d43d5b41e8cb
ASJ <dkstmdwl0615@naver.com>
2024-03-06 10:54:31 +0900

    Title: 격자 모양 출력하기, Time: 199ms, Memory: 47MB, Status: Passed - CodeTree

---
+++ 240306/격자 모양 출력하기/print-grid-shape.js
@@ -0,0 +1,24 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split("\n");
+
+const [n, m] = input[0].split(" ").map(Number)
+
+const matrix = Array.from(Array(n), () => Array(n).fill(0));
+
+const placed = []
+
+for(let i = 1; i <= m; i++){
+    placed.push(input[i].split(" ").map(Number))
+}
+
+for(let i = 0; i < placed.length; i++){
+    matrix[placed[i][0] - 1][placed[i][1] - 1] = placed[i][0] * placed[i][1]
+}
+
+for(let i = 0; i < n; i++){
+    let str = ""
+    for(let j = 0; j < n; j++){
+        str += matrix[i][j] + " "
+    }
+    console.log(str)
+}
