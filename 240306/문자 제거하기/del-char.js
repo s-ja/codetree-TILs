5a1b926343c34ea05201a9d3fd707ebbb50fb12b
ASJ <dkstmdwl0615@naver.com>
2024-03-06 16:50:08 +0900

    Title: 문자 제거하기, Time: 261ms, Memory: 14MB, Status: Passed - CodeTree

--- 240306/문자 제거하기/del-char.js
+++ 240306/문자 제거하기/del-char.js
@@ -8,9 +8,12 @@ const arr = input.map(Number)
 // console.log(str, arr)
 
 for(let i = 0; i < arr.length; i++){
-    str.splice(arr[i],1)
     if(arr[i] >= str.length){
         str.pop()
     }
+    str.splice(arr[i],1)
     console.log(str.join(""))
+    if(str.length === 1){
+        break
+    }
 }
