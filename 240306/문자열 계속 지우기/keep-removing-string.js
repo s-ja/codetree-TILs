a9eceeb16c17d98ce65447190dc8c02b862d3b68
ASJ <dkstmdwl0615@naver.com>
2024-03-06 17:59:33 +0900

    Title: 문자열 계속 지우기, Time: 178ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240306/문자열 계속 지우기/keep-removing-string.js
+++ 240306/문자열 계속 지우기/keep-removing-string.js
@@ -1,23 +1,28 @@
 const fs = require("fs");
 let input = fs.readFileSync(0).toString().trim().split("\n");
 
-const a = input[0].split("")
+let a = input[0].split("")
 const b = input[1].split("")
 
 for(let i = 0; i < a.length; i++){
+    if(a.join("") === b.join("")){
+        a = []
+        break
+    }
+    
     let tmp = ""
     for(let j = 0; j < b.length; j++){
         if(a[i + j] === b[j]){
             tmp += a[i + j]
         }
     }
-    // console.log(tmp)
+    // console.log("j 반복문을 다 돌고 나서 쌓인 tmp : " + tmp)
     if(tmp === b.join("")){
-        // console.log(i)
+        // console.log("tmp 와 b가 일치할 때의 i : " + i)
         a.splice(i, b.length)
         i = 0
     }
-    // console.log(a)
+    // console.log("사이클이 한번 돌고 난 후의 a : " + a)
 }
 
 console.log(a.join(""))
