9d82a1b9fd7b2f083bbd4002d8c2154f026f902a
ASJ <dkstmdwl0615@naver.com>
2024-03-06 12:15:35 +0900

    Title: 특정 문자로 끝나는 문자열, Time: 211ms, Memory: 14MB, Status: Passed - CodeTree

--- 240306/특정 문자로 끝나는 문자열/string-ending-with-specific-character.js
+++ 240306/특정 문자로 끝나는 문자열/string-ending-with-specific-character.js
@@ -3,8 +3,15 @@ const input = fs.readFileSync(0).toString().trim().split("\n");
 
 const char = input.pop()
 
+let cnt = 0;
+
 for(let i = 0; i < input.length; i++){
     if(input[i][input[i].length - 1] === char){
         console.log(input[i])
+        cnt++
     }
+}
+
+if(cnt === 0){
+    console.log("None")
 }
