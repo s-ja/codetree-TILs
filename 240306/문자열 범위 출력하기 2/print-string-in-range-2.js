96b77b3851cf33f662d72edcd63df54b8f2f8a26
ASJ <dkstmdwl0615@naver.com>
2024-03-06 12:30:22 +0900

    Title: 문자열 범위 출력하기 2, Time: 192ms, Memory: 14MB, Status: Passed - CodeTree

--- 240306/문자열 범위 출력하기 2/print-string-in-range-2.js
+++ 240306/문자열 범위 출력하기 2/print-string-in-range-2.js
@@ -6,7 +6,9 @@ const num = Number(input[1])
 let str = ""
 
 for(let i = input[0].length - 1; i >= input[0].length - num; i--){
-    str += input[0][i]
+    if(input[0][i]){
+        str += input[0][i]
+    }
 }
 
 console.log(str)
