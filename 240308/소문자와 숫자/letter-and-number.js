0701d8325e715be9c5630b5119bfe3a577f5bfab
ASJ <dkstmdwl0615@naver.com>
2024-03-08 19:11:01 +0900

    Title: 소문자와 숫자, Time: 232ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240308/소문자와 숫자/letter-and-number.js
@@ -0,0 +1,17 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split("");
+
+let str = ""
+let result = ""
+
+for(a of input){
+    str += a.toLowerCase()
+}
+
+for(a of str.split("")){
+    if((a >= "a" && a <= "z") || (a >= "1" && a <= "9")){
+        result += a
+    }
+}
+
+console.log(result)
