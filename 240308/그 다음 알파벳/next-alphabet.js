ef7d53e575805f749bce2c824794b2d84bfaacea
ASJ <dkstmdwl0615@naver.com>
2024-03-08 18:39:19 +0900

    Title: 그 다음 알파벳, Time: 181ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240308/그 다음 알파벳/next-alphabet.js
+++ 240308/그 다음 알파벳/next-alphabet.js
@@ -1,4 +1,15 @@
 const fs = require("fs");
 const input = fs.readFileSync(0).toString().trim();
 
-console.log(String.fromCharCode(input.charCodeAt(0) + 1))
+// console.log(String.fromCharCode(input.charCodeAt(0) + 1))
+
+let charCode = input.charCodeAt(0)
+
+// console.log(charCode)
+// console.log("a".charCodeAt(0))
+
+if(charCode = 122){
+    charCode = 96
+}
+
+console.log(String.fromCharCode(charCode + 1))
