2fe964d8807ed1e79cb4228f76f4b3e33658943e
ASJ <dkstmdwl0615@naver.com>
2024-03-27 15:27:23 +0900

    Title: 함수를 이용한 소수 판별, Time: 186ms, Memory: 14MB, Status: Passed - CodeTree

--- 240327/함수를 이용한 소수 판별/decimal-decisions-using-functions.js
+++ 240327/함수를 이용한 소수 판별/decimal-decisions-using-functions.js
@@ -12,6 +12,10 @@ for(let i = a; i <= b; i++){
 }
 
 function isPrime(n){
+    if(n === 1){
+        return false
+    }
+
     for(let i = 2; i < n; i++){
         if(n % i === 0){
             return false;
