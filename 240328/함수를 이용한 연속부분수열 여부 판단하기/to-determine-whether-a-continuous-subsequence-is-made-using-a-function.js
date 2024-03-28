4560737d9a9ad806be84b6d4ad8fe21ea060ea1c
ASJ <dkstmdwl0615@naver.com>
2024-03-28 19:52:12 +0900

    Title: 함수를 이용한 연속부분수열 여부 판단하기, Time: 193ms, Memory: 15MB, Status: Wrong Answer - CodeTree

--- 240328/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.js
+++ 240328/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.js
@@ -12,27 +12,41 @@ function result(input){
         return "No"
     }
 
-    let result = false
+    let result = []
 
     for(let i = 0; i < n1 - n2 + 1; i++){
-        let tmp = false;
+        let tmp = [];
         for(let j = 0; j < n2; j++){
             if(A[i + j] !== B[j]){
                 // result = false
                 break
+            }else{
+                tmp.push(A[i + j])
             }
-            result = true;
-            tmp = true;
         }
 
-        if(tmp){
-            break
+        result.push(tmp)
+    }
+    let cnt = 0
+
+    for(elem of result){
+        for(let i = 0; i < elem.length; i++){
+            if(elem[i] === B[i]){
+                cnt++
+            }else{
+                break
+            }
         }
     }
 
-    if(result){
+    // return result
+    if(cnt === B.length){
         return "Yes"
     }else{
-        return "No"
+        "No"
     }
 }
+
+// 95 21
+// 76 80 21 82 62 13 3 8 53 67 2 90 74 6 15 64 40 67 30 72 67 41 72 61 6 2 70 10 13 29 98 22 12 13 89 93 48 72 11 40 96 33 16 9 23 52 26 96 90 88 70 87 25 20 4 99 8 53 20 14 20 58 46 27 69 82 43 79 70 48 66 64 60 29 48 90 50 21 78 77 95 19 15 89 14 98 55 96 50 58 59 83 14 93 72 
+// 69 49 21 66 23 37 90 15 28 10 35 98 82 35 61 83 24 85 17 91 71
