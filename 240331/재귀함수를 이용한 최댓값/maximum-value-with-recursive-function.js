8697eb3ba202958a806bb1fe1d016d2b0d06c15b
ASJ <dkstmdwl0615@naver.com>
2024-03-31 11:20:26 +0900

    Title: 재귀함수를 이용한 최댓값, Time: 5070ms, Memory: 22MB, Status: Time Limit Exceed - CodeTree

---
+++ 240331/재귀함수를 이용한 최댓값/maximum-value-with-recursive-function.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+let input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+
+const n = Number(input[0])
+const arr = input[1].split(" ").map(Number)
+
+console.log(solution(arr))
+
+function solution(arr){
+    if(arr.length === 1){
+        return arr[0]
+    }
+
+    if(arr[0] > solution(arr.slice(1))){
+        return arr[0]
+    }else{
+        return solution(arr.slice(1))
+    }
+}
