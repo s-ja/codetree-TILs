62eab3a60d6402eb1a80f037fca7cc2e4e31ad04
ASJ <dkstmdwl0615@naver.com>
2024-04-02 18:17:35 +0900

    Title: 그 요일은, Time: 186ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240402/그 요일은/the-day-of-the-day.js
@@ -0,0 +1,42 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const [m1, d1, m2, d2] = input[0].split(" ").map(Number)
+const weekDay = input[1]
+
+const week  = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+
+const thirtyOne = [1, 3, 5, 7, 8, 10, 12]
+
+function elapsed(m, d){
+    let elapsedDay = 0
+
+    for(let i = 1; i < m; i++){
+        if(m === 2){
+            elapsedDay += 29
+        }else if(thirtyOne.includes(m)){
+            elapsedDay += 31
+        }else{
+            elapsedDay += 30
+        }
+    }
+
+    elapsedDay += d
+    return elapsedDay
+}
+
+// console.log(elapsed(m1, d1), elapsed(m2, d2))
+const diff = elapsed(m2, d2) - elapsed(m1, d1)
+const index = week.indexOf(weekDay)
+// console.log(diff)
+// console.log(parseInt(diff / 7), diff % 7)
+// console.log(index)
+let cnt = 0
+
+for(let i = 1; i < diff; i++){
+    if(i % 7 === index){
+        cnt++
+    }
+}
+
+console.log(cnt)
