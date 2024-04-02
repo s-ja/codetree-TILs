066fd4eb9ed35ea14678effa45724950600811ad
ASJ <dkstmdwl0615@naver.com>
2024-04-02 16:11:00 +0900

    Title: Date to Date, Time: 176ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240402/Date to Date/date-to-date.js
@@ -0,0 +1,35 @@
+const fs = require("fs");
+let input = fs.readFileSync("/dev/stdin").toString().trim().split(" ").map(Number);
+
+const [m1, d1, m2, d2] = input
+
+let elapsed1 = 1
+let elapsed2 = 1
+
+const thirtyone = [1, 3, 5, 7, 8, 9, 10, 12]
+
+for(let i = 0; i < m2; i++){
+    if(i === 2){
+        elapsed1 += 28
+    }else if(thirtyone.includes(i)){
+        elapsed1 += 31
+    }else{
+        elapsed1 += 30
+    }
+}
+
+elapsed1 += d2
+
+for(let i = 0; i < m1; i++){
+    if(i === 2){
+        elapsed2 += 28
+    }else if(thirtyone.includes(i)){
+        elapsed2 += 31
+    }else{
+        elapsed2 += 30
+    }
+}
+
+elapsed2 += d1
+
+console.log(elapsed1 - elapsed2 + 1)
