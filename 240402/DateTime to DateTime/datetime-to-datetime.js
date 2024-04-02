aa1033e4a0960851d08200ca43af0376387a2ca9
ASJ <dkstmdwl0615@naver.com>
2024-04-02 17:12:33 +0900

    Title: DateTime to DateTime, Time: 187ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240402/DateTime to DateTime/datetime-to-datetime.js
+++ 240402/DateTime to DateTime/datetime-to-datetime.js
@@ -6,19 +6,17 @@ const [a, b, c] = input
 //2011년 11월 11일 11시 11분
 //2011년 11월 a일 b시 c분
 
-function calc(a, b, c){
-    if(a < 11){
-        return -1
-    }else if(a === 11 && b < 11){
-        return -1
-    }else if(a === 11 && b < 11 && c < 11){
-        return -1
+function calc(a, b, c) {
+    if (a === 11 && b === 11 && c < 11) {
+        return -1;
     }
 
-    const days = a - 11
-    const hours = (days * 24) - 11 + b
-    const minutes = (hours * 60) - 11 + c
-    return minutes
+    const daysDiff = a - 11;
+    const hoursDiff = b - 11;
+    const minutesDiff = c - 11;
+
+    const totalMinutes = (daysDiff * 24 * 60) + (hoursDiff * 60) + minutesDiff;
+    return totalMinutes;
 }
 
 // 1일  = 24 시간  = 60분 * 24 시간 = 1440 분
