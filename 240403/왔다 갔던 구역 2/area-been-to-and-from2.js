9af62c210e8b70e97a328a02312d8b22ea32ffec
ASJ <dkstmdwl0615@naver.com>
2024-04-03 16:27:21 +0900

    Title: 왔다 갔던 구역 2, Time: 171ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240403/왔다 갔던 구역 2/area-been-to-and-from2.js
+++ 240403/왔다 갔던 구역 2/area-been-to-and-from2.js
@@ -4,32 +4,52 @@ let input = fs.readFileSync("/dev/stdin").toString().trim().split('\n');
 // console.log(input)
 
 const n = Number(input.shift())
+const commands = []
+input.forEach((command) => {
+    const [leng, dir] = command.split(" ")
+    const length = Number(leng)
+    commands.push([length, dir])
+})
+// console.log(commands)
 
 const arr = Array(n * 10).fill(0)
-let curr = (n * 10) / 2
-// console.log(arr.length, curr)
-let cnt = 0
+let pointer = (n * 10) / 2
+let acc = 0
 
-for(elem of input){
-    const [leng, dir] = elem.split(' ')
-    const length = Number(leng)
-    for(let i = 0; i < length; i++){
-        arr[curr]++
-        // console.log('command start : ' + arr)
-        // console.log('command start : ' + curr)
-
-        if(dir === "L"){
-            curr--
-            // console.log('curr : ' + curr)
-        }else{
-            curr++
-            // console.log('curr : ' + curr)
+for(const command of commands){
+    if(acc < 1){
+        // console.log("before : " + pointer)
+        for(let i = 0; i < command[0]; i++){
+            if(command[1] === "L"){
+                pointer--
+            }else{
+                pointer++
+            }
+            // console.log("pointer : " + pointer)
+            arr[pointer]++
+            // console.log(arr)
         }
-        // console.log(curr)
+        // console.log(arr)
+        // console.log("after : " + pointer)
+        acc++
+    }else{
+        // console.log("before : " + pointer)
+        for(let i = 0; i < command[0]; i++){
+            arr[pointer]++
+            if(command[1] === "L"){
+                pointer--
+            }else{
+                pointer++
+            }
+            // console.log("pointer : " + pointer)
+            // console.log(arr)
+        }
+        // console.log("after : " + pointer)
+        acc++
     }
-    // console.log("cut")
-    // console.log('command end : ' + arr)
 }
+
+let cnt = 0
 
 for(elem of arr){
     if(elem > 1){
@@ -38,4 +58,3 @@ for(elem of arr){
 }
 
 console.log(cnt)
-// console.log(arr)
