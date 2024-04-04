08a9e8710a49123fa604351f9a873ad3346ede2c
ASJ <dkstmdwl0615@naver.com>
2024-04-04 11:39:29 +0900

    Title: 사각형의 총 넓이 2, Time: 373ms, Memory: 82MB, Status: Passed - CodeTree

---
+++ 240404/사각형의 총 넓이 2/total-width-of-a-rectangle2.js
@@ -0,0 +1,40 @@
+const fs = require("fs");
+let input = fs.readFileSync("/dev/stdin").toString().trim().split('\n');
+
+const n = Number(input.shift());
+const offSet = 100;
+const maxN = 10;
+const maxPoint = 100 + offSet;
+const matrix = []
+
+for(let i = 0; i < maxN * maxPoint + 1; i++){
+    const tmp = []
+    for(let i = 0; i < maxN * maxPoint + 1; i++){
+        tmp.push(0)
+    }
+    matrix.push(tmp)
+}
+
+for(const elem of input){
+    let [x1, y1, x2, y2] = elem.split(' ').map(x => Number(x) + offSet);
+
+    for(let i = x1; i < x2; i++){
+        for(let j = y1; j < y2; j++){
+            matrix[i][j] = 1;
+        }
+    }
+}
+
+// console.log(matrix)
+
+let area = 0;
+
+for(const row of matrix){
+    for(const col of row){
+        if(col === 1){
+            area++;
+        }
+    }
+}
+
+console.log(area)
